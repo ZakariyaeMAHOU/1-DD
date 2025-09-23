7dddf77a710c7f24a1b37a31610d9a9b18fa1ea4
unknown <zakariyae.ista@gmail.com>
2025-09-23 18:17:21 +0100

    React

--- DEV OWFS 201/M204 Développement front-end/2-local/tp1_react/src/index.js
+++ DEV OWFS 201/M204 Développement front-end/2-local/tp1_react/src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const p1 = <p>Berkane</p>
+const style1 = {
+    listStyleType: "none",
+    color: "red"
+}
+const style2 = {
+    listStyleType: "square",
+    color: "blue"
+}
+const stagiaire = ['anas', 'amine', 'karim', 'samir'];
+const clr = ['red', 'blue', 'orange', 'tomato'];
+///////////////////////////////////////////////////////////////////////
+const liste =
+    <ul
+        id='liste1'
+        // className='red' 
+        // style={{listStyleType:"none",color:"blue"}}
+        style={style1}
+    >
+        {
+            // stagiaire.map(s => <li>{s}</li> )
+            stagiaire.map((s, index) => <li key={index}>{s}</li>)
+        }
+    </ul>
+////////////////////////////////////////////////////////////////////////
+// const ListeElements = function () { // compesant
+//     return <ul
+//         id='liste1'
+//         // className='red' 
+//         // style={{listStyleType:"none",color:"blue"}}
+//         style={style1}
+//     >
+//         {
+//             // stagiaire.map(s => <li>{s}</li> )
+//             stagiaire.map((s, index) => <li key={index}>{s}</li>)
+//         }
+//     </ul>
+// }
+const ListeElements = function (props) { // compesant
+    return (
+        <ul
+            id='liste1'
+            style={props.style}
+        >
+            {
+                props.arr.map((s, index) => <li key={index}>{s}</li>)
+            }
+        </ul>
+    )
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+        <ListeElements arr={stagiaire} style={style1} />
+        <ListeElements arr={clr} style={style2} />
+    </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
