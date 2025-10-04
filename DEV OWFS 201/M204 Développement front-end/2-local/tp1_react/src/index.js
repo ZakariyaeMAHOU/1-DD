321af4004a092860f80ce7fa471b9be320f21c87
unknown <zakariyae.ista@gmail.com>
2025-10-04 10:47:41 +0100

    M204 React Js

--- DEV OWFS 201/M204 Développement front-end/2-local/tp1_react/src/index.js
+++ DEV OWFS 201/M204 Développement front-end/2-local/tp1_react/src/index.js
@@ -42,25 +42,25 @@ const laptop = [
     id: 1,
     title: "PC Portable Gamer HP VICTUS",
     price: "7490 DH",
-    thumbnail: "HP16D0195NF.jpg",
+    thumbnail: "pc1.jpg",
   },
   {
     id: 2,
     title: "PC Portable Gamer HP VICTUS",
     price: "2190 DH",
-    thumbnail: "HP14424U3EA.jpg",
+    thumbnail: "pc2.jpg",
   },
   {
     id: 3,
     title: "Pc Portable Chromebook Acer",
     price: "3640 DH",
-    thumbnail: "NXATHEF002.jpg",
+    thumbnail: "pc3.jpg",
   },
   {
     id: 4,
     title: "PC Portable - HUAWEI",
     price: "1270 DH",
-    thumbnail: "HUA6901443442959.jpg",
+    thumbnail: "pc4.jpg",
   },
 ];
 const voitures = [
